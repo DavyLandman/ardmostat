ce283b32686e29c3f0c1f2b07d37fab9d0fdd2dc
Davy Landman <davy.landman+uva@gmail.com>
2012-02-11 18:45:52 +0100

    Added decryption to the temperature sending

--- Server/temperature_log/logtemp.js
+++ Server/temperature_log/logtemp.js
@@ -1,5 +1,17 @@
 var net = require('net');
 var orm = require('orm');
+var crypto = require('crypto');
+
+var key = new Buffer([
+		0xEE, 0xCC, 0x3C, 0x4D,
+		0x31, 0xF9, 0xB2, 0xDA,
+		0xBE, 0xEF, 0x37, 0xA2,
+		0x43, 0x42, 0x55, 0x76
+	]).toString('binary');
+
+var iv = '';
+
+
 var db = orm.connect('postgresql://node_temp:report@localhost/Thermostat', function( success, db) {
 		if (!success) {
 			console.log('Could not connect the database!\n\t%d:%s', db.number, db.message);
@@ -13,9 +25,12 @@ var db = orm.connect('postgresql://node_temp:report@localhost/Thermostat', funct
 
 		var server = net.createServer(function(c) {
 			c.on('data', function (data) {
-				var dataBuf = new Buffer(data);
-				if (dataBuf.length == 4) {
-					var tempSend = dataBuf.readFloatLE(0);
+				if (data.length == 16) {
+					data = new Buffer(data.toString('binary') + 'paddingbug' , 'binary');
+					var decrypter = crypto.createDecipheriv('AES-128-ECB', key, iv);
+					var result = decrypter.update(data); // first block is all we need
+					var dataBuf = new Buffer(result);
+					var tempSend = dataBuf.readUInt8(0) + (dataBuf.readUInt8(1) / 100);
 					console.log('temperature: ' + tempSend);
 					new Temp({ "occurance": new Date() , "temperature": tempSend }).save(function (err) {
 						if (err) {
