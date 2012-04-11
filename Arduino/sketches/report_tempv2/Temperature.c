900a4a95d56bbc23c8540f07564c911874017c8b
Davy Landman <davy.landman@gmail.com>
2012-04-11 19:30:06 +0200

    Implemented most part of the temperature log

--- Arduino/sketches/report_tempv2/Temperature.c
+++ Arduino/sketches/report_tempv2/Temperature.c
@@ -1,32 +1,52 @@
 #include "Temperature.h"
+#include "debuglog.h"
+#define SMOOTHAMOUNT 10
+#define SMOOTHINTERVAL 500UL
+#define INTERVAL (60000UL - (SMOOTHAMOUNT * SMOOTHINTERVAL))
+
+static SharedState sharedState;
+static double measuredTemperature;
+static uint32_t nextMeasureTime;
+
 void Temperature_initialize(SharedState state ) {
+	sharedState = state;
+	nextMeasureTime = millis() + INTERVAL;
 }
 
 void Temperature_FillSharedState() {
+	sharedState->currentTemperature = measuredTemperature / SMOOTHAMOUNT;
 }
 
+static int smoothCount;
+static uint32_t nextSampleTime;
 void Temperature_InitializeSmoothing() {
+	smoothCount = 0;
+	nextSampleTime = nextMeasureTime + SMOOTHINTERVAL;
+	measuredTemperature = 0.0;
 }
 
 void Temperature_PrepareNextMeasurementRound() {
+	nextMeasureTime += INTERVAL;
 }
 
 void Temperature_PrepareNextSmoothStep() {
+	nextSampleTime += SMOOTHINTERVAL;
 }
 
 void Temperature_SampleTemperature() {
+	smoothCount++;
 }
 
 uint8_t _Temperature_con_EnoughSamplesCollected() {
-	return 1;
+	return smoothCount >= SMOOTHAMOUNT;
 }
 
 uint8_t _Temperature_con_ShouldSmooth() {
-	return 1;
+	return millis() >= nextSampleTime;
 }
 
 uint8_t _Temperature_con_ShouldStartMeasurement() {
-	return 1;
+	return millis() >= nextMeasureTime;
 }
 
 	
